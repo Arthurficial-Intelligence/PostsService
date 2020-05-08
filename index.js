a0cfd3ba1c86ed39598aa51d1046b4ba67762620
arthur rankin smith <arthurrankin1992@gmail.com>
2020-05-08 12:26:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
-const cors = require('cors');
-
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,16 +14,29 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = {
     id,
     title,
   };
+  await axios.post("http://localhost:4005/events", {
+    type: "POST_CREATED",
+    data: {
+      id,
+      title,
+    },
+  });
 
-  res.status(201).send(posts[id])
+  res.status(201).send(posts[id]);
 });
+
+app.post('/events', (req, res) => {
+  console.log('recieved event', req.body.type);
+
+  res.send({})
+})
 
 app.listen(4000, () => {
   console.log("listening on 4000");
